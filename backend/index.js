0c58bf2024b6f48d6f639a57838b8ea9f6cf8d71
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2025-05-27 19:32:15 -0400

    Refactor: Add .env for MongoDB URI and update index.js for database connection

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); 
+
 const port = 4000;
 const express = require("express");
 const app = express();
@@ -12,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection with MongoDB
-mongoose.connect("mongodb+srv://alia14:Tatum2021@cluster0.fu1rrod.mongodb.net/comp495")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Successfully Connected"))
   .catch((error) => 
     {
