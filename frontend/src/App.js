d106daf8c6ee96004cf42cf5f985f83f34f0b98c
Spencer Teillon <teillo01@email.franklin.edu>
2025-06-10 19:58:00 -0400

    Issue #9 and #10 - Created Cart.jsx and Login.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './Components/Navbar/Navbar';
 import Shop from './Pages/Shop';
+import Login from './Pages/Login';
+import Cart from './Pages/Cart';
 // MORE IMPORTS WILL GO HERE.
 
 import './App.css';
@@ -14,9 +16,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Shop />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Cart" element={<Cart />} />
         {/* Add your routes here, example: <Route path="/about" element={<About />} /> */}
-        {/*<Route path="/cart"
-        {/*<Route path="/login" 
         {/*<Route path="/register"
         {/* Add more routes as needed */}
       </Routes>
---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>Cart</h1>
+            <p>Items</p>
+        </div>
+    );
+}
