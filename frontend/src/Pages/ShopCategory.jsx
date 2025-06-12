49912d2b282046f0f86ee4c1c58a1a95f9d6806b
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2025-06-12 15:28:26 -0400

    fixed typo in ShopContext and ShopCategory code

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -7,7 +7,7 @@ import Item from '../Components/Item/Item';
 
 const ShopCategory = (props) => {
 
-  const {all_products} = useContext(ShopContext);
+  const {all_product} = useContext(ShopContext);
 
   return (
     <div className='shop-category'>
@@ -21,7 +21,7 @@ const ShopCategory = (props) => {
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_products.map((item, i)=> {
+        {all_product.map((item, i)=> {
             if (props.category == item.category) {
               return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
             }
