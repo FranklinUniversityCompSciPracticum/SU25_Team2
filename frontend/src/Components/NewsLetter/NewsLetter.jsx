1b903fecc830850b6ee38dbc74036e20bccaa897
Spencer Teillon at doco <teillo01@email.franklin.edu>
2025-06-13 14:05:57 -0400

    made NewsLetter.css submission box scale width

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -6,8 +6,8 @@ const NewsLetter = () => {
         <div className='newsletter'>
             <h1>Get Exclusive Offers On Your Email</h1>
             <p>Subscribe and Save!</p>
-            <div>
-                <input type="email" placeholder='Your email ID'/>
+            <div className='submit'>
+                <input type="email" placeholder='Your email ID' className='submit'/>
                 <button>Subscribe</button>
             </div>
         </div>
