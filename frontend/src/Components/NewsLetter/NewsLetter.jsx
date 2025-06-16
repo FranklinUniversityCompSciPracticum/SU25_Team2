345d2c28534b5acf84ab946be830f541b2ef14cc
Spencer Teillon at doco <teillo01@email.franklin.edu>
2025-06-16 15:37:02 -0400

    Updated NewsLetter component to be closeable and stay locked at bottom of the screen

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './NewsLetter.css'
 
-const NewsLetter = () => {
+const NewsLetter = ({ onClose }) => {
     return (
         <div className='newsletter'>
-            <h1>Get Exclusive Offers On Your Email</h1>
-            <p>Subscribe and Save!</p>
-            <div className='submit'>
-                <input type="email" placeholder='Your email ID' className='submit'/>
-                <button>Subscribe</button>
+            <div className="background-white">
+                <div className="background-trans">
+                <button className="close-button" onClick={onClose}>Not now</button>
+                <h1>Get Exclusive Offers On Your Email</h1>
+                <p>Subscribe and Save!</p>
+                    <div className='submit'>
+                        <input type="email" placeholder='Your email ID' className='submit'/>
+                        <button>Subscribe</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
