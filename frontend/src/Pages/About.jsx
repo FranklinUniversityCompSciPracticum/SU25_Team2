f45a2dc5d545efc1616acabd86569a0e76dec848
Spencer Teillon <teillo01@email.franklin.edu>
2025-06-19 20:27:43 -0400

    added about us page

---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur tempore debitis doloremque alias, incidunt quisquam magni ratione labore omnis vitae totam facere minima vero, ipsa eos blanditiis. Enim, praesentium quam!</p>
+        </div>
+    );
+}
