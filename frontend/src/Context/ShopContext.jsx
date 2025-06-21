960291fa6871d0fa821595e26e01fece5f470b6d
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2025-06-20 20:02:09 -0400

    updated ShopContext with cart logic, added ProductAddedModal, and implemented dynamic cart count in navbar

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,69 @@
-import React, { createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import all_product from "../Components/Assets/all_products"
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let product of all_product) {
+    cart[product.id] = 0;
+  }
+  return cart;
+};
+
+
 const ShopContextProvider = (props)=>{
-  const contextValue = {all_product};
+
+  const [cartItems,setCartItems] = useState(getDefaultCart());
+  const [showModal, setShowModal] = useState(false);
+ 
+
+
+  const addToCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+    setShowModal(true);
+    setTimeout(() => {
+    setShowModal(false);
+  }, 2000); // Hide after 2 seconds
+  }
+
+   const removeFromCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  }
+
+  const getTotalCartAmount = () => {
+  let totalAmount = 0;
+  for (const item in cartItems) {
+    if (cartItems[item] > 0) {
+      let itemInfo = all_product.find((product) => product.id === Number(item));
+      if (itemInfo) {
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+    }
+  }
+  return totalAmount;
+}
+
+
+const getTotalCartItems = () => {
+  let totalItem = 0;
+  for (const item in cartItems)
+  {
+    if (cartItems[item]>0)
+    {
+      totalItem += cartItems[item];
+    }
+  }
+  return totalItem;
+}
+
+
+   const contextValue = {getTotalCartItems, getTotalCartAmount, all_product,cartItems,addToCart,removeFromCart, showModal};
+
+ 
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
 
   return (
     <ShopContext.Provider value={contextValue}>
