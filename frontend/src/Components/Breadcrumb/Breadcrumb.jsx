4a59fbd97a6327ced3a69c1742fc6f86bd387af0
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2025-06-20 20:11:27 -0400

    Fixed breadcrumb links to use <Link> instead of <a> to keep cart count

--- frontend/src/Components/Breadcrumb/Breadcrumb.jsx
+++ frontend/src/Components/Breadcrumb/Breadcrumb.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Breadcrumb.css';
 import arrow_icon from '../Assets/breadcrum_arrow.png';
 
@@ -8,9 +9,9 @@ const Breadcrumb = (props) => {
     return (
         <nav className='breadcrumb'>
             <ul>
-                <li><a href="/">HOME</a></li>
-                <li><a href="/">SHOP</a></li>
-                <li><a href={`/${product.category.toLowerCase()}`}>{product.category.toUpperCase()}</a></li>
+                <li><Link to="/">HOME</Link></li>
+                <li><Link to="/">SHOP</Link></li>
+                <li><Link to={`/${product.category.toLowerCase()}`}>{product.category.toUpperCase()}</Link></li>
                 <li><span className="current">{product.name}</span></li>
             </ul>
         </nav>
