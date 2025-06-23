294b85f30c95d07670a8f0e95126bd0dce90b66b
cstarcher1 <Starch20@email.franklin.edu>
2025-06-23 14:03:23 -0400

    final version of new collection

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import Item from '../Item/Item'
 import './NewCollections.css'
 import new_collections from '../Assets/new_collections'
 const NewCollections = () => {
     return (
-        <div className= 'newcollections'>
+        <div className= "newcollections">
             <h1>NEW COLLECTIONS</h1>
             <hr />
             <div className= "collections">
                 {new_collections.map((item,i)=>{
-                    return <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
                 })}
             </div>
         </div>
