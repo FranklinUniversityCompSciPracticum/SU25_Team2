bc1125ced2ebb9595178887ac959fcd01ad35236
Spencer Teillon at doco <teillo01@email.franklin.edu>
2025-06-27 16:29:17 -0400

    added register button to /login

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     return (
         <div style={{ padding: '20px', textAlign: 'center' }}>
             <h1>Login</h1>
             <p>Input credentials</p>
+            <Link to='/register' className="register-button"><button>Register</button></Link>
         </div>
     );
 }
