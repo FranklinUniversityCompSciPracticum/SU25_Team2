34ce8f9a4eae2f9be3e64c20507e8ac5a7a27e28
Spencer Teillon <teillo01@email.franklin.edu>
2025-06-29 18:56:51 -0400

    Register page changed button and link colors

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -12,6 +12,7 @@ const Register = ( ) => {
             component="form"
             sx={{ '& > :not(style)': { m: 1, width: '40ch'} }}
         >
+            <h1 className='register-title'>Sign up to stay connected!</h1>
             <div className='name-entry'>
                 <TextField className='entry-field' label="First Name" variant="outlined" />
                 <TextField className='entry-field' label="Last Name" variant="outlined" />
@@ -20,8 +21,8 @@ const Register = ( ) => {
             <TextField className='entry-field' label="Username" variant="outlined" />
             <TextField className='entry-field' label="Password" variant="outlined" type="password" />
             <TextField className='entry-field' label="Verify Password" variant="outlined" type="password" />
-            <Button variant="contained">Register</Button>
-            <p className='login-redirect'>Already have an account? Login <Link to='/login'>here</Link> instead</p>
+            <Button className='register-button' variant="contained">Register</Button>
+            <p className='login-redirect'>Already have an account? Login <Link className='login-link' to='/login'>here</Link> instead</p>
         </Box>
     )
 }
