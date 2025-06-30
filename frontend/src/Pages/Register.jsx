dd4273118cf37e6892dab0289e1570599a5ceae9
Spencer Teillon <teillo01@email.franklin.edu>
2025-06-29 20:00:12 -0400

    Register page: added checkboxes for terms of use and privacy policy

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { FormGroup, FormControlLabel, Checkbox} from '@mui/material';
 
 const Register = ( ) => {
     return (
@@ -21,8 +22,26 @@ const Register = ( ) => {
             <TextField className='entry-field' label="Username" variant="outlined" />
             <TextField className='entry-field' label="Password" variant="outlined" type="password" />
             <TextField className='entry-field' label="Verify Password" variant="outlined" type="password" />
+            <FormGroup className='checkbox-group'>
+                <FormControlLabel required control={<Checkbox />} label={
+                    <span>
+                        I agree to the{' '}
+                            <Link className='link' to="/terms">
+                                Terms and Conditions
+                            </Link>
+                        </span>
+                     } />
+                <FormControlLabel required control={<Checkbox />} label={
+                    <span>
+                        I agree to the{' '}
+                            <Link className='link' to="/privacy">
+                                Privacy Policy
+                            </Link>
+                        </span>
+                     } />
+            </FormGroup>
             <Button className='register-button' variant="contained">Register</Button>
-            <p className='login-redirect'>Already have an account? Login <Link className='login-link' to='/login'>here</Link> instead</p>
+            <p className='login-redirect'>Already have an account? Login <Link className='link' to='/login'>here</Link> instead</p>
         </Box>
     )
 }
