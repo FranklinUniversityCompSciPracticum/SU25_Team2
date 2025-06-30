b445e23cca05a73f451f54759cfaba8599e9a000
Spencer Teillon <teillo01@email.franklin.edu>
2025-06-29 20:15:06 -0400

    added error message if both check boxes are not clicked

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './CSS/Register.css'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -7,6 +7,10 @@ import { Link } from 'react-router-dom';
 import { FormGroup, FormControlLabel, Checkbox} from '@mui/material';
 
 const Register = ( ) => {
+    const [agreedToTerms, setAgreedToTerms] = useState(false);
+    const [agreedToPrivacy, setAgreedToPrivacy] = useState(false);
+    const [showError, setShowError] = useState(false);
+
     return (
         <Box 
             className='registration-form'
@@ -23,24 +27,59 @@ const Register = ( ) => {
             <TextField className='entry-field' label="Password" variant="outlined" type="password" />
             <TextField className='entry-field' label="Verify Password" variant="outlined" type="password" />
             <FormGroup className='checkbox-group'>
-                <FormControlLabel required control={<Checkbox />} label={
-                    <span>
-                        I agree to the{' '}
-                            <Link className='link' to="/terms">
-                                Terms and Conditions
-                            </Link>
-                        </span>
-                     } />
-                <FormControlLabel required control={<Checkbox />} label={
-                    <span>
-                        I agree to the{' '}
-                            <Link className='link' to="/privacy">
-                                Privacy Policy
-                            </Link>
-                        </span>
-                     } />
+                <FormControlLabel
+                    required
+                    control={
+                        <Checkbox
+                            checked={agreedToTerms}
+                            onChange={(e) => setAgreedToTerms(e.target.checked)}
+                        />} 
+                        label={
+                            <span>
+                                I agree to the{' '}
+                                    <Link className='link' to="/terms">
+                                        Terms and Conditions
+                                    </Link>
+                                </span>
+                            } 
+                        />
+                <FormControlLabel
+                    required
+                    control={
+                        <Checkbox 
+                            checked={agreedToPrivacy}
+                            onChange={(e) => setAgreedToPrivacy(e.target.checked)}
+                        />}
+                    label={
+                        <span>
+                            I agree to the{' '}
+                                <Link className='link' to="/privacy">
+                                    Privacy Policy
+                                </Link>
+                            </span>
+                        } 
+                    />
             </FormGroup>
-            <Button className='register-button' variant="contained">Register</Button>
+            <Button
+                className='register-button'
+                variant="contained"
+                sx={{ backgroundColor: '#009688', '&:hover': { backgroundColor: '#00796B' } }}
+                onClick={(e) => {
+                    e.preventDefault();
+                    if (!agreedToTerms || !agreedToPrivacy) {
+                        setShowError(true);
+                    } else {
+                        setShowError(false);
+                    }
+                }}
+            >
+                Register
+            </Button>
+            {showError && (
+                <p className="error-message">
+                    You must agree to the Terms and Privacy Policy to register.
+                </p>
+            )}
             <p className='login-redirect'>Already have an account? Login <Link className='link' to='/login'>here</Link> instead</p>
         </Box>
     )
