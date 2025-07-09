73d3d35aa7f39fc98025ca61c24664750fdf9c36
Spencer Teillon at doco <teillo01@email.franklin.edu>
2025-07-09 12:18:43 -0400

    navbar ready for styling

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,6 @@ import navProfile from '../../assets/nav-profile.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        Navbar
         <img src={navlogo} alt="" className="nav-logo" />
         <img src={navProfile} alt="" className="nav-profile"/>
     </div>
