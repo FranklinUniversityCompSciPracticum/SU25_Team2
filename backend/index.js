8105a5b99a6684ff0525c647f58b6db183ad067c
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2025-07-22 18:51:39 -0400

    updated implementation of NewCollection endpoint and frontend API

--- backend/index.js
+++ backend/index.js
@@ -267,5 +267,5 @@ app.get('/newcollections',async (req,res)=> {
 	let products = await Product.find({});
 	let newcollection = products.slice(1).slice(-8);
 	console.log("NewCollection Fetched");
-	res.send(newcollection);
+	res.json(newcollection);
 })
